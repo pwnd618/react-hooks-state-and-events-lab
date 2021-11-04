4225e0757a2a26a7cc7838d80155a200190aa858
webster <wbian618@gmail.com>
2021-11-03 22:59:22 -0400

    commit tree 48346cefba0b6501d1ac07a77d81767b5759f89d

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [currentClassName, setCurrentClassName] = useState(true)
+  const cartUpdate = currentClassName ? "Add to Cart" : "Remove From Cart"
+  function changeCart() {
+  setCurrentClassName(cart => !cart)
+  }
+
   return (
-    <li className="">
+    <li className={currentClassName ? '' : "in-cart"}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={currentClassName ? "add" : "remove"} onClick={changeCart}>{cartUpdate}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [filterBy, setFilterBy] = useState("All")
+function handleFilter(e) {
+  setFilterBy(e.target.value)
+}
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
